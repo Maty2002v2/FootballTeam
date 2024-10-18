d9fb01b5e8c5f0420b8c46a31a866fbb3ef4850c
Maty2002v2 <matikster21@gmail.com>
2024-10-18 15:55:44 +0200

    Create basic user table

---
+++ src/components/UsersTable.vue
@@ -0,0 +1,104 @@
+<template>
+    <div v-if="users.length" class="wrapper">
+        <div class="table">
+            <div class="table_row">
+                <div class="table_header table__cell"></div>
+                <div class="table_header table__cell">Full Name</div>
+                <div class="table_header table__cell">Actions</div>
+            </div>
+
+            <div v-for="user in users" :key="user.id" class="table_row">
+                <div class="table__cell">
+                    <img :src="user.avatar" alt="User avatar" class="table__cell-avatar">
+                </div>
+                <div class="table__cell">{{ user.first_name + user.last_name }}</div>
+                <div class="table__cell table__actions">
+                    <FilePenLine class="table__icon" color="#ABB2BA"/>
+                    <Trash class="table__icon" color="#ABB2BA"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { FilePenLine, Trash } from 'lucide-vue-next';
+import type { User } from '../types';
+
+defineProps<{
+    users: User[]
+}>();
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    margin: 0px 10px;
+    padding: 20px;
+    // box-shadow: ;
+    border-radius: 6px;
+    background-color: #FFFFFF;
+    
+    @include breakpoint(md) {
+        width: 1135px;
+        max-width: 95vw;
+        margin: 0;
+    }
+}
+
+.table {
+    color: #494F62;
+    max-width: 100vw;
+    overflow-x: scroll;
+
+    @include breakpoint(md) {
+        max-width: unset;
+        overflow-x: auto;
+    }
+    
+    &_row {
+        display: grid;
+        grid-template-columns: 80px 400px 70px;
+        text-align: start;
+        
+        @include breakpoint(md) {
+            grid-template-columns: 40px 1fr 70px; 
+            gap: 40px;
+        }
+
+        &:nth-child(even) {
+            background-color: #F5F7F9;
+        }
+    }
+
+    &_header {
+        font-weight: 700;
+    }
+
+    &__cell {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        background-color: inherit;
+
+        &-avatar {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+        }
+    }
+
+    &__actions {
+        display: flex;
+        gap: 10px;
+    }
+
+    &__icon {
+        cursor: pointer;
+        width: 20px;
+        height: 20px;
+    }
+}
+</style>
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        Users
+        <UsersTable :users="users" />
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import UsersTable from '../components/UsersTable.vue';
+import { useReqresApi } from '../composables/useReqresApi';
+
+const { users, getUsers } = useReqresApi();
+getUsers(1);
+</script>
