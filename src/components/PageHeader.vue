ddeece94a21d55a4b1e2f473db482d2a09901e83
Maty2002v2 <matikster21@gmail.com>
2024-10-20 16:08:50 +0200

    Create PageHeader component and use it in app

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="page-header"><slot></slot></h1>
+</template>
+
+<style lang="scss" scoped>
+.page-header {
+    color: #4A4F56;
+    font-size: 2.5em;
+    font-weight: 300;
+}
+</style>
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,6 +1,6 @@
 <template>
     <PageBox>
-        <h1>Users list</h1>
+        <PageHeader>Users list</PageHeader>
 
         <div v-if="usersData && !isLoading">
             <UsersTable :usersData="usersData"/>
@@ -12,6 +12,7 @@
 
 <script lang="ts" setup>
 import PageBox from '../components/PageBox.vue';
+import PageHeader from '../components/PageHeader.vue';
 import UsersTable from '../components/UsersTable.vue';
 import Loader from '../components/Loader.vue';
 import { useReqresApi } from '../composables/useReqresApi';
