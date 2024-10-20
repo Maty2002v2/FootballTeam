7287987bf08da7c5697f96b84feeb6a42e63ace8
Maty2002v2 <matikster21@gmail.com>
2024-10-20 18:37:38 +0200

    Create basic Add user page

--- src/pages/CreateUser.vue
+++ src/pages/CreateUser.vue
@@ -1,7 +1,48 @@
 <template>
-    <div>
-        Create User
-    </div>
+    <PageBox>
+        <h1>Add user</h1>
+
+        <div class="create-user">
+            <Box class="create-user-box">
+                <form class="form">
+                    <div class="form__inputs">
+                        <Input label="First Name"/>
+                        <Input label="Last Name" />
+                    </div>
+
+                    <Button type="button">Create user</Button>
+                </form>
+            </Box>
+
+            <Box class="create-user-box">
+            </Box>
+        </div>
+    </PageBox>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import PageBox from '../components/PageBox.vue';
+import Box from '../components/Box.vue';
+import Button from '../components/Button.vue';
+import Input from '../components/Input.vue';
+</script>
+
+<style lang="scss" scoped>
+.create-user {
+    display: grid;
+    grid-template-columns: 66% 33%;
+    gap: 30px;
+
+    &-box {
+        padding-top: 60px;
+    }
+}
+
+.form {
+    &__inputs {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 30px;
+    }
+}
+</style>
