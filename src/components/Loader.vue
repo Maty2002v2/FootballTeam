46f8477d3166233debbaa28a3421b9372ee93a3a
Maty2002v2 <matikster21@gmail.com>
2024-10-20 13:19:40 +0200

    Create loader and change background color

---
+++ src/components/Loader.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="loader"></div>
+</template>
+
+<style lang="scss" scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+
+  &::before {
+      content: "";
+      width: 48px;
+      height: 48px;
+      box-sizing: border-box;
+      position: absolute;
+      inset: 0px;
+      border-radius: 50%;
+      border: 5px solid #459571;
+      animation: prixClipFix 2s linear infinite ;
+    }
+}
+
+@keyframes rotation {
+  0% {
+      transform: rotate(0deg);
+  }
+  100% {
+      transform: rotate(360deg);
+  }
+}
+
+@keyframes prixClipFix {
+    0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+    25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+    50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+    75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+    100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
+}
+</style>
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,13 +1,16 @@
 <template>
-    <div v-if="usersData">
+    <div v-if="usersData && !isLoading">
         <UsersTable :usersData="usersData"/>
     </div>
+
+    <Loader v-else />
 </template>
 
 <script lang="ts" setup>
 import UsersTable from '../components/UsersTable.vue';
+import Loader from '../components/Loader.vue';
 import { useReqresApi } from '../composables/useReqresApi';
 
-const { usersData, getUsers } = useReqresApi();
+const { usersData, isLoading, getUsers } = useReqresApi();
 getUsers();
 </script>
