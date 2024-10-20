cfaaf04a8616981c09a92e3e482f7ffcce47ff95
Maty2002v2 <matikster21@gmail.com>
2024-10-20 19:03:16 +0200

    Change styles CreateUser page for mobile

--- src/pages/CreateUser.vue
+++ src/pages/CreateUser.vue
@@ -1,6 +1,6 @@
 <template>
     <PageBox>
-        <h1>Add user</h1>
+        <PageHeader>Add user</PageHeader>
 
         <div class="create-user">
             <Box class="create-user-box">
@@ -10,12 +10,12 @@
                         <Input label="Last Name" />
                     </div>
 
-                    <Button type="button">Create user</Button>
+                    <Button type="button" class="form__button">Create user</Button>
                 </form>
             </Box>
 
             <Box class="create-user-box">
-                <LoadAvatarTile /> 
+                <LoadAvatarTile />
             </Box>
         </div>
     </PageBox>
@@ -23,6 +23,7 @@
 
 <script lang="ts" setup>
 import PageBox from '../components/PageBox.vue';
+import PageHeader from '../components/PageHeader.vue';
 import Box from '../components/Box.vue';
 import Button from '../components/Button.vue';
 import Input from '../components/Input.vue';
@@ -32,8 +33,12 @@ import LoadAvatarTile from '../components/LoadAvatarTile.vue';
 <style lang="scss" scoped>
 .create-user {
     display: grid;
-    grid-template-columns: 66% 33%;
+    grid-template-columns: 1fr;
     gap: 30px;
+
+    @include breakpoint(md) {
+        grid-template-columns: 66% 33%;
+    }
 
     &-box {
         padding-top: 60px;
@@ -41,10 +46,24 @@ import LoadAvatarTile from '../components/LoadAvatarTile.vue';
 }
 
 .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+
     &__inputs {
         display: grid;
-        grid-template-columns: 1fr 1fr;
-        gap: 30px;
+        grid-template-columns: 1fr;
+        gap: 20px;
+
+        @include breakpoint(sm) {
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+
+    &__button {
+        width: fit-content;
+        margin-top: 20px;
     }
 }
 </style>
